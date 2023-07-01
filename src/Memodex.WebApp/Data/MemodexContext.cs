20b75742b8d26100f2f6cb6439b23974f04c98b8
kamgru <kamil.grudzien@hotmail.com>
2023-07-01 17:48:02 +0200

    add profile handling
    added creating and selecting profile and storing it in session

--- src/Memodex.WebApp/Data/MemodexContext.cs
+++ src/Memodex.WebApp/Data/MemodexContext.cs
@@ -2,16 +2,17 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Memodex.WebApp.Data;
 
-public class MemodexContext : DbContext
-{
+public class MemodexContext : DbContext 
+{ 
     public MemodexContext(
         DbContextOptions<MemodexContext> options)
         : base(options)
     {
-    }
-
+    } 
+    
     public DbSet<Flashcard> Flashcards => Set<Flashcard>();
     public DbSet<Category> Categories => Set<Category>();
+    public DbSet<Profile> Profiles => Set<Profile>();
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
---
+++ src/Memodex.WebApp/Data/Profile.cs
@@ -0,0 +1,7 @@
+namespace Memodex.WebApp.Data;
+
+public class Profile
+{
+    public int Id { get; set; }
+    public required string Name { get;set; }
+}
