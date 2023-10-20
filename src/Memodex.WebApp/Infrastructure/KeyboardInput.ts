c9da862c8667ba459994ac35bc6551ec1cc12362
kamgru <kamil.grudzien@hotmail.com>
2023-10-20 16:16:24 +0200

    fix keyboard input when running with a base path

--- src/Memodex.WebApp/Infrastructure/KeyboardInput.ts
+++ src/Memodex.WebApp/Infrastructure/KeyboardInput.ts
@@ -1,20 +1,25 @@
-const contextName: string = document.location.pathname.split('/')[1]
-    .toLowerCase();
+let contextName: string | undefined;
+
+const contextElement: HTMLElement | null = document.querySelector('[data-input-context]');
+
+if (contextElement !== null) {
+    contextName = contextElement.dataset.inputContext;
+}
 
 if (contextName === 'engage') {
 
     const form = document.getElementById('engage-controls-form') as HTMLFormElement;
-    if (form == null) {
+    if (!form) {
         throw new Error('Could not find engage-controls-form');
     }
 
     const flashcardEngage = document.querySelector('flashcard-engage') as FlashcardEngage;
-    if (flashcardEngage == null) {
+    if (!flashcardEngage) {
         throw new Error('Could not find FlashcardEngage');
     }
 
     const markReviewCheckbox = document.getElementById('engage-controls-mark-checkbox') as HTMLInputElement;
-    if (markReviewCheckbox == null) {
+    if (!markReviewCheckbox) {
         throw new Error('Could not find engage-controls-mark-checkbox');
     }
 
@@ -24,6 +29,26 @@ if (contextName === 'engage') {
         }
         if (event.key === 'm') {
             markReviewCheckbox.checked = !markReviewCheckbox.checked;
+        }
+        if (event.key === 'r') {
+            flashcardEngage.toggleAnswer();
+        }
+    });
+} else if (contextName === 'review') {
+    
+    const form = document.getElementById('review-controls-form') as HTMLFormElement;
+    if (!form) {
+        throw new Error('Could not find review-controls-form');
+    }
+
+    const flashcardEngage = document.querySelector('flashcard-engage') as FlashcardEngage;
+    if (!flashcardEngage) {
+        throw new Error('Could not find FlashcardEngage');
+    }
+
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'n') {
+            form.submit();
         }
         if (event.key === 'r') {
             flashcardEngage.toggleAnswer();
